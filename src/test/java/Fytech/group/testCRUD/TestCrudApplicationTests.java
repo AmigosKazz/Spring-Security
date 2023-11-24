1361c5538652ff314f1da4f95e8cbee054fd90c6
Kaznarah <kaznarahandrinarivo@gmail.com>
2023-11-24 23:28:47 +0300

    security v.1

---
+++ src/test/java/Fytech/group/testCRUD/TestCrudApplicationTests.java
@@ -0,0 +1,14 @@
+package Fytech.group.testCRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
