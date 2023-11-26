94584c162fc520388b92e9ba99578fbaffa2de2c
Kaznarah1 <kaznarahandrinarivo@gmail.com>
2023-11-26 13:19:12 +0300

    Service config

--- src/main/java/Fytech/group/testCRUD/confirguration/SpringSecurityConfig.java
+++ src/main/java/Fytech/group/testCRUD/confirguration/SpringSecurityConfig.java
@@ -1,11 +1,16 @@
 package Fytech.group.testCRUD.confirguration;
 
 
+import Fytech.group.testCRUD.service.CustomUserDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -16,6 +21,9 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SpringSecurityConfig {
+
+    @Autowired
+    public CustomUserDetailService customUserDetailService;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -44,6 +52,24 @@ public class SpringSecurityConfig {
     @Bean
     public BCryptPasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(){
+        return new AuthenticationManager() {
+            @Override
+            public Authentication authenticate(Authentication authentication) {
+                return null;
+            }
+        };
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager (HttpSecurity http, BCryptPasswordEncoder bCryptPasswordEncoder) throws Exception{
+        AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(customUserDetailService).passwordEncoder(bCryptPasswordEncoder);
+
+        return authenticationManagerBuilder.build();
     }
 
 }
--- src/main/java/Fytech/group/testCRUD/service/CustomUserDetailService.java
+++ src/main/java/Fytech/group/testCRUD/service/CustomUserDetailService.java
@@ -1,13 +1,38 @@
 package Fytech.group.testCRUD.service;
 
+import Fytech.group.testCRUD.model.DBUser;
+import Fytech.group.testCRUD.repo.DBUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class CustomUserDetailService implements UserDetailsService {
+
+    @Autowired
+    private DBUserRepository dbUserRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        DBUser user = dbUserRepository.findByUsername(username);
+
+        return new User(user.getUsername(),user.getPassword(), null);
+
     }
+
+    private List<GrantedAuthority> getGrantedAuthorities(String role){
+        List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+        authorities.add(new SimpleGrantedAuthority("ROLE_"+role));
+
+        return authorities;
+    }
+
 }
